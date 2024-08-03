d8ff83ad090312fc2b1c2dc5ab1637f2a78c1645
Muhammad Daniyal <daniyalghani159@gmail.com>
2024-08-04 02:34:43 +0500

    variables

---
+++ 01_Basics/01_variavles.js
@@ -0,0 +1,31 @@
+const accountId = 2143;
+let acountEmail = "daniyal@gmial.com";
+var accountPassword = 1234235;
+acountCity = "Karachi";
+let acountCountry;
+
+// Single Line Comment
+// console.log(accountId);
+// console.table([accountId, acountEmail, accountPassword, acountCity]);
+
+// Changing values
+
+// accountId = 34224;  // not allowed
+acountEmail = "subhan@gmial.com";
+accountPassword = 903849385;
+acountCity = "Hyderabad";
+
+console.table([
+  accountId,
+  acountEmail,
+  accountPassword,
+  acountCity,
+  acountCountry,
+]);
+
+// Multi Line Comment
+
+/* 
+prefer not to use var 
+because of issue in block scope and function scope
+*/
