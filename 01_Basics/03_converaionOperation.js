7b9f03207702d835c3c4106a876b0f0f9731a819
Muhammad Daniyal <daniyalghani159@gmail.com>
2024-08-04 08:51:41 +0500

    operation and conversion

---
+++ 01_Basics/03_converaionOperation.js
@@ -0,0 +1,79 @@
+// let score = 33;
+
+// // console.log(typeof score);
+// // console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// // console.log(valueInNumber);
+// // console.log(typeof valueInNumber);
+
+// // "33" => 33 & type = number
+// // "daniyal" => NaN & type = number
+// // true = 1 & false = 0 & type = number
+
+// let isLoggedIn = "";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// // console.log(booleanIsLoggedIn);
+
+// // false = 0 and true = 1
+// // "" => false
+// // "daniyal" => true
+
+// let number = 0;
+
+// let stringNumber = String(num1);
+
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber);
+
+// // false values = 0, null , undefined, NaN , "", false
+// // true value = all values except above values
+
+// let = var1 = 14314;
+
+// let booleanVar = Boolean(var1);
+// // console.log(booleanVar);
+
+// // *********************** Operations ***********************
+
+// let value = 3;
+// let negValue = -value;
+// // console.log(negValue);
+
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2**3);
+// // console.log(2/3);
+// // console.log(2%3);
+
+// let str1 = "hello";
+// let str2 = " hitesh";
+
+// let str3 = str1 + str2;
+// // console.log(str3);
+
+// // console.log("1" + 2);
+// // console.log(1 + "2");
+// // console.log("1" + 2 + 2);   code runs left to right and can use brackets  to set priority
+// console.log(1 + 2 + "2"); // 32
+// console.log(1 + (2 + "2")); // 122
+
+// // console.log( (3 + 4) * 5 % 3);
+
+// // console.log(+true);
+// // console.log(+"");
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 2 + 2;
+
+// let gameCounter = 100;
+// console.log(gameCounter);
+// ++gameCounter;
+// console.log(gameCounter);
+
+// // link to study
+// // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
