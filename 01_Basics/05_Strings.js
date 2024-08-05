dd1c0bfe8865bf74647e2ae78b6938aa9ca579a3
Muhammad Daniyal <daniyalghani159@gmail.com>
2024-08-05 22:25:35 +0500

    strings methods

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,41 @@
+const name = "daniyal";
+const repoCount = 20;
+
+// console.log("my name is " + name + " and my repo count is " + repoCount);
+// console.log(`my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Assassins-Creed-BlackFlag"); // this is object not array
+// console.log(gameName[0]);
+// console.log(gameName[1]);
+// console.log(gameName[2]);
+// console.log(gameName[3]);
+
+// console.log(gameName.length);
+// console.log(gameName.charAt(20));
+// console.log(gameName.indexOf("C"));
+// const capitalString = gameName.toUpperCase();
+// console.log(capitalString);
+
+// const newString = gameName.substring(0, 9);
+// const newString2 = gameName.substring(10, 15);
+// console.log(newString);
+// console.log(newString2);
+
+// const userInput = "       Ali Ahmed            ";
+// console.log(userInput.trim());
+
+const url = "https://github.com/daniyal-rashid?tab=repositories";
+// console.log(url.replace("?", "/"));
+// console.log(url.includes("daniyal"));
+
+// console.log(gameName.split("-")); // return array
+
+// const str = "The morning is upon us.";
+// str.slice(-3); // 'us.'
+// str.slice(-3, -1); // 'us'
+// str.slice(0, -1); // 'The morning is upon us'
+// str.slice(4, -1); // 'morning is upon us'
+
+// console.log(gameName.italics()); // output => <i>Assassins-Creed-BlackFlag</i>
+// console.log(gameName.startsWith("Creed")); // return true or false
+// console.log(gameName.link()); // output => <a href="undefined">Assassins-Creed-BlackFlag</a>
