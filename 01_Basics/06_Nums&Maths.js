69aa074735836b0e15f2c6e05523963339a07018
Muhammad Daniyal <daniyalghani159@gmail.com>
2024-08-06 21:01:29 +0500

    maths and numbers

---
+++ 01_Basics/06_Nums&Maths.js
@@ -0,0 +1,37 @@
+const score = 400;
+// console.log(score);
+
+let balance = new Number(100);
+// balance = "daniyal";
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966;
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// ++++++++++++++++++  Maths ++++++++++++++++
+
+// console.log(Math);
+// console.log(typeof Math);
+// console.log(Math.abs(-10));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(3.7));
+// console.log(Math.floor(3.7));
+// console.log(Math.min(3, 5, 7, 4, 0));
+// console.log(Math.max(3, 5, 7, 4, 0));
+// console.log(Math.PI);
+// console.log(Math.sqrt(2));
+// console.log(Math.cos(0));
+
+// console.log(Math.floor(Math.random() * 10 + 1));
+
+const min = 10;
+const max = 20;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
